9869efa2a50f085c72d0ac615dac5cd3c74191a5
Nicolas Payot <nicolas.payot@gmail.com>
2023-09-27 21:16:22 +0200

    feat(nuxt-jobs): add part 1.1

---
+++ nuxt-jobs/tailwind.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ['./components/**/*.vue', './layouts/**/*.vue', './pages/**/*.vue', './app.vue', './node_modules/auth-module/src/**/*.vue'],
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
